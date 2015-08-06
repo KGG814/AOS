52826278d4ae2706b64d443543f37a7028d81557
Kalana Gamlath <kalana.gamlath1@gmail.com>
2015-08-06 16:59:03 +1000

    .

--- libs/libclock/src/clock.c
+++ libs/libclock/src/clock.c
@@ -1,56 +1,66 @@
 #include <clock/clock.h>
 
-#include <platsupport/timer.h>
-#include <platsupport/plat/timer.h>
+/* 
+ * GPT Registers
+ */
+#define GPT_PADDR       0x02098000
 
-#define PRESCALE 		0
-#define GPT_STATUS_REGISTER_CLEAR 0x3F
-static pstimer_t singleton_timer;
-static gpt_t singleton_gpt;
+#define GPT_CR          (char*)(GPT_PADDR + 0x00)
+#define GPT_PR          (char*)(GPT_PADDR + 0x04)
+#define GPT_SR          (char*)(GPT_PADDR + 0x08)
+#define GPT_IR          (char*)(GPT_PADDR + 0x0C)
+#define GPT_OCR1    (char*)(GPT_PADDR + 0x10)
+#define GPT_OCR2        (char*)(GPT_PADDR + 0x14)
+#define GPT_OCR3    (char*)(GPT_PADDR + 0x18)
+#define GPT_CNT     (char*)(GPT_PADDR + 0x24)
+
+/*
+ * GPT_CR Bitmasks
+ */
+
+#define EN                      0x00000001
+#define ENMOD           0x00000002
+#define CLKSRC          0x000001C0
+#define FRR                     0x00000200
+#define OM_ALL          0x1FF00000
+#define IM_ALL          0x000F0000
+#define SWR                     0x00008000
+
+#define PG_CLK          0x00000080
+
+/*
+ * GPT_CR Bitmasks
+ */
+
+ #define IR_ALL         0x0000003F
 /*
  * Initialise driver. Performs implicit stop_timer() if already initialised.
  *    interrupt_ep:       A (possibly badged) async endpoint that the driver
                           should use for deliverying interrupts to
  *
  * Returns CLOCK_R_OK iff successful.
-
-
  */
 int start_timer(seL4_CPtr interrupt_ep) {
-	struct gpt_map gpt_map;
-
-	pstimer_t *timer = &singleton_timer;
-    gpt_t *gpt = &singleton_gpt;
-
-    /***/
-    timer->properties.upcounter = true;
-    timer->properties.timeouts = false;
-    timer->properties.bit_width = 32;
-    timer->properties.irqs = 1;
-
-    timer->data = (void *) gpt;
-
-    gpt->gpt_map = (volatile struct gpt_map*)&gpt_map;
-    gpt->prescaler = PRESCALE;
-
-    /* Disable GPT. */
-    /* Sets EN, OM#, IM# to 0 */
-    gpt->gpt_map->gptcr = 0;
-    /* Set interrupt register to 0 */
-    gpt->gpt_map->gptir = 0;
-    gpt->gpt_map->gptsr = GPT_STATUS_REGISTER_CLEAR;
-
-    /* Configure GPT. */
-    /* Reset to 0 on disable */
-    gpt->gpt_map->gptcr |= BIT(ENMOD); 
-    /* Reset the GPT */
-    gpt->gpt_map->gptcr = 0 | BIT(SWR); 
-    gpt->gpt_map->gptcr = BIT(FRR) | BIT(CLKSRC) | BIT(ENMOD); /* GPT can do more but for this just
-            set it as free running  so we can tell the time */
-    //gpt->gpt_map->gptir = BIT(ROV); /* Interrupt when the timer overflows */
-    gpt->gpt_map->gptpr = PRESCALE; /* Set the prescaler */
-    gpt->gpt_map->gptcr = BIT(EN);
-	return 0;
+        /* Disable the GPT */
+        *GPT_CR &= ~EN;
+        /* Set all writable GPT_IR fields to zero*/
+        *GPT_IR &= ~IR_ALL;
+        /* Configure Output mode to disconnected, write zeros in OM3, OM2, OM1 *
+        *GPT_CR &= ~OM_ALL;
+        /* Disable Input Capture Modes*/ 
+        *GPT_CR &= ~IM_ALL;
+        /* Change clock source to PG_CLK */
+        *GPT_CR &= ~PG_CLK;
+        /* Assert SWR bit */
+        assert(*GPT_CR & SWR == SWR);
+        /* Clear GPT status register (set to clear) */
+        *GPT_SR = 0xFFFFFFFF;
+        /* Make sure the GPT starts from 0 when we start it */
+        *GPT_CR &= ENMOD;
+        /* Enable the GPT */
+        *GPT_CR &= EN;
+        (void*) interrupt_ep;
+        return 0;
 }
 
 /*
@@ -59,15 +69,8 @@ int start_timer(seL4_CPtr interrupt_ep) {
  * Returns a negative value if failure.
  */
 timestamp_t time_stamp(void) {
-	pstimer_t *timer = &singleton_timer;
-    gpt_t *gpt = (gpt_t*) timer->data;
-    uint64_t value;
-
-    value = gpt->gpt_map->gptcnt;
-    //uint64_t ns = (value / (uint64_t)IPG_FREQ) * NS_IN_US * (gpt->prescaler + 1);
-    return value;
+        return *GPT_CNT;
 }
-
 /*\
  * Register a callback to be called after a given delay
  *    delay:  Delay time in microseconds before callback is invoked
@@ -76,26 +79,7 @@ timestamp_t time_stamp(void) {
  *
  * Returns 0 on failure, otherwise an unique ID for this timeout
  */
-//uint32_t register_timer(uint64_t delay, timer_callback_t callback, void *data);
+//uint32_t register_timer(uint64_t delay, timer_callback_t callback, void *data)
 
 /*
- * Remove a previously registered callback by its ID
- *    id: Unique ID returned by register_time
- * Returns CLOCK_R_OK iff successful.
- */
-//int remove_timer(uint32_t id);
-
-/*
- * Handle an interrupt message sent to 'interrupt_ep' from start_timer
- *
- * Returns CLOCK_R_OK iff successful
- */
-//int timer_interrupt(void);
-
-
-/*
- * Stop clock driver operation.
- *
- * Returns CLOCK_R_OK iff successful.
- */
-//int stop_timer(void);
+:
