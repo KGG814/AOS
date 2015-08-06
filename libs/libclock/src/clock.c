48436fea9eddf740a3e87a5d86c03c6f200e5816
Kalana Gamlath <kalana.gamlath1@gmail.com>
2015-08-06 17:04:50 +1000

    .

--- libs/libclock/src/clock.c
+++ libs/libclock/src/clock.c
@@ -1,4 +1,5 @@
 #include <clock/clock.h>
+#include <mapping.h>
 
 /* 
  * GPT Registers
@@ -9,22 +10,22 @@
 #define GPT_PR          (char*)(GPT_PADDR + 0x04)
 #define GPT_SR          (char*)(GPT_PADDR + 0x08)
 #define GPT_IR          (char*)(GPT_PADDR + 0x0C)
-#define GPT_OCR1    (char*)(GPT_PADDR + 0x10)
+#define GPT_OCR1    	(char*)(GPT_PADDR + 0x10)
 #define GPT_OCR2        (char*)(GPT_PADDR + 0x14)
-#define GPT_OCR3    (char*)(GPT_PADDR + 0x18)
-#define GPT_CNT     (char*)(GPT_PADDR + 0x24)
+#define GPT_OCR3    	(char*)(GPT_PADDR + 0x18)
+#define GPT_CNT     	(char*)(GPT_PADDR + 0x24)
 
 /*
  * GPT_CR Bitmasks
  */
 
-#define EN                      0x00000001
+#define EN              0x00000001
 #define ENMOD           0x00000002
 #define CLKSRC          0x000001C0
-#define FRR                     0x00000200
+#define FRR             0x00000200
 #define OM_ALL          0x1FF00000
 #define IM_ALL          0x000F0000
-#define SWR                     0x00008000
+#define SWR             0x00008000
 
 #define PG_CLK          0x00000080
 
@@ -41,25 +42,8 @@
  * Returns CLOCK_R_OK iff successful.
  */
 int start_timer(seL4_CPtr interrupt_ep) {
-        /* Disable the GPT */
-        *GPT_CR &= ~EN;
-        /* Set all writable GPT_IR fields to zero*/
-        *GPT_IR &= ~IR_ALL;
-        /* Configure Output mode to disconnected, write zeros in OM3, OM2, OM1 *
-        *GPT_CR &= ~OM_ALL;
-        /* Disable Input Capture Modes*/ 
-        *GPT_CR &= ~IM_ALL;
-        /* Change clock source to PG_CLK */
-        *GPT_CR &= ~PG_CLK;
-        /* Assert SWR bit */
-        assert(*GPT_CR & SWR == SWR);
-        /* Clear GPT status register (set to clear) */
-        *GPT_SR = 0xFFFFFFFF;
-        /* Make sure the GPT starts from 0 when we start it */
-        *GPT_CR &= ENMOD;
-        /* Enable the GPT */
-        *GPT_CR &= EN;
-        (void*) interrupt_ep;
+        char* gpt = map_device(GPT_ADDR, GPT_SIZE);
+
         return 0;
 }
 
