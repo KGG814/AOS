b140fcb5c8ccc077356cbbec5b832f40e8b09abb
hCoder <hcse542@cse.unsw.EDU.AU>
2015-08-18 17:55:18 +1000

    asbdklfj

---
+++ apps/sos/src/proc_vspace_layout.h
@@ -0,0 +1,11 @@
+
+//start of code/data segment 
+
+#define CODE_DATA_START 0x10000000
+
+#define HEAP_OFFSET 0x100000 
+
+//full decreasing stack 
+#define STACK_TOP 0xe0000000
+
+#define IPC_BUF_START STACK_TOP
