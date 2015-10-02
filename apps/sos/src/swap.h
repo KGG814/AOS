9b2b63e544fb9f64ff136d6eb12f224f899691c2
Howard Seatang <howard.seatang@gmail.com>
2015-10-02 08:58:55 +0000

    added ifndef guards to swap

--- apps/sos/src/swap.h
+++ apps/sos/src/swap.h
@@ -1,5 +1,9 @@
-#include <sel4/types.h>
+#ifndef _SWAP_H_
+#define _SWAP_H_
 
+#include <sel4/types.h>
 
 int swap_init(void);
 void write_to_swap_slot(int pid, seL4_CPtr reply_cap, void *args);
+
+#endif /* _SWAP_H_ */
